265e4da6d8be59efbc9aaeb8c1e9f848ffe4dbe1
Tony <tony.c13579@gmail.com>
2018-03-16 10:27:10 -0700

    v1.00

---
+++ osc.h
@@ -0,0 +1,10 @@
+/* file osc.h                                                */
+/*                                                           */
+/* adapted from July 2006 osc.h by Mike Coombes              */
+/* function moved to osc.c  Nov. 7, 2006 (by Dan Peirce)     */
+/* Set Clock speed to 32 MHz                                 */
+/*                                                           */
+void set_osc_32MHz(void); 
+void set_osc_8MHz(void); 
+void set_osc_1MHz(void); 
+void set_osc_500KHz(void); 
